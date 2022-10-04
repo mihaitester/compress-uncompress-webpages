5b297528990bd8c409f92a4b7fed4d128f26cdda
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-10-04 14:16:22 +0300

    fix(script): added default usage for uncompressing files

---
+++ unzip-webpages.cmd
@@ -0,0 +1,4 @@
+@echo off
+rem default use script that will dump the `.cache` file containing metadata and the `.json` file containing duplicates
+rem replace `%*` with a drive path like `c:\` or `d:\` or include both `c:\ d:\` or pass them through the `.cmd` script
+python zip-unzip-webpages.py -u -d %*
